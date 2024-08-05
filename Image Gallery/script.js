ac744a6f698b291bb3eb1e48b495483c9327ec9e
Indrajeet Mondal <indrajeetmondal120703@gmail.com>
2024-08-05 14:27:08 +0530

    Completed

--- Image Gallery/script.js
+++ Image Gallery/script.js
@@ -5,24 +5,58 @@ const overlayClose = overlay.querySelector(".close");
 
 function generateHTML([h, v]) {
   return `
-    <div class="item h${h} v${v}">
-      <img src="images/${randomNumber(12)}.jpg">
-      <div class="item__overlay">
-        <button>View -></button>
-      </div>
-    </div>
-  `;
+        <div class="item h${h} v${v}">
+          <img src="images/${randomNumber(12)}.jpg">
+          <div class="item__overlay">
+            <button>View →</button>
+          </div>
+        </div>
+      `;
 }
 
 function randomNumber(limit) {
   return Math.floor(Math.random() * limit) + 1;
 }
 
+function handleClick(e) {
+  const src = e.currentTarget.querySelector("img").src;
+  overlayImage.src = src;
+  overlay.classList.add("open");
+}
+
+function close() {
+  overlay.classList.remove("open");
+}
+
 const digits = Array.from({ length: 50 }, () => [
   randomNumber(4),
   randomNumber(4),
+]).concat([
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
 ]);
 
 const html = digits.map(generateHTML).join("");
-
 gallery.innerHTML = html;
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item) => item.addEventListener("click", handleClick));
+
+overlayClose.addEventListener("click", close);
